98aeeb4de4a6daa95851169c028825a6b683a8ce
DezaRealdy <deza.realdy@gmail.com>
2022-10-04 13:08:36 +0200

    Neuen und besseren tacho implementiert und eine Methode erstellt die den Tachozeiger je nach eingestetzten Wert bewegt.

--- app/app/src/main/java/com/example/foodcompass/MainActivity.java
+++ app/app/src/main/java/com/example/foodcompass/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.foodcompass;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -11,7 +12,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.ekn.gruzer.gaugelibrary.HalfGauge;
+
+import com.github.anastr.speedviewlib.SpeedView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.journeyapps.barcodescanner.CaptureActivity;
 import com.journeyapps.barcodescanner.ScanContract;
@@ -19,43 +21,50 @@ import com.journeyapps.barcodescanner.ScanOptions;
 
 public class MainActivity extends AppCompatActivity {
 
-    /*HalfGauge tacho;
-    com.ekn.gruzer.gaugelibrary.Range rangeOne, rangeTwo, rangeThree;
-    int setGraphic;*/
+    int setGraphic;
     FloatingActionButton btn_scan;
+    CardView lunchCard;
+    int nutriScore;
+    int healthyScore = 20;
+    SpeedView tacho;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.lunch_food_activity);
+        setContentView(R.layout.activity_main);
 
-        btn_scan = findViewById(R.id.scannerButton);
+        tacho = findViewById(R.id.tacho);
+        displayNutriScoreOnTacho(healthyScore);
+
+        lunchCard = findViewById(R.id.mittagessen_cardView);
+        lunchCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LunchAddActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        /*btn_scan = findViewById(R.id.);
         btn_scan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, LunchAddActivity.class);
                 startActivity(intent);
             }
-        });
+        });*/
 
 
-        //Tacho anzeige hier werden die Bereiche festgelegt (Farbe, Werte, etc.)
 
-        /*rangeOne = new com.ekn.gruzer.gaugelibrary.Range();
-        rangeTwo = new com.ekn.gruzer.gaugelibrary.Range();
-        rangeThree = new com.ekn.gruzer.gaugelibrary.Range();
 
-        rangeOne.setFrom(0);rangeOne.setTo(100);
-        rangeTwo.setFrom(100);rangeTwo.setTo(200);
-        rangeThree.setFrom(200);rangeThree.setTo(300);
+        private int nutriScoreCalculator(int FoodNutriScore) {
+            
+        }
 
-        rangeOne.setColor(Color.GREEN);
-        rangeTwo.setColor(Color.BLUE);
-        rangeThree.setColor(Color.RED);
-        tacho.setMinValue(0);
-        tacho.setMaxValue(300);
-        tacho.setValue(150);*/
+
+
 
         /*Barcode scanner per click auf ScanBtn
         btn_scan = findViewById(R.id.scannerButton);
@@ -93,5 +102,11 @@ public class MainActivity extends AppCompatActivity {
     });
 
          */
+    }
+
+    private void displayNutriScoreOnTacho (int healthyScore) {
+        this.healthyScore = healthyScore;
+
+        tacho.speedTo((float)healthyScore);
     }
 }
